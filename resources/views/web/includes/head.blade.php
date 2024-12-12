e62ae19ca270bd143a0345d2b2fe6465f1fdf3ca
Mansuriya Brij <48708476+brijmansuriya@users.noreply.github.com>
2024-12-12 20:51:37 +0530

    remove cdn

--- resources/views/web/includes/head.blade.php
+++ resources/views/web/includes/head.blade.php
@@ -12,9 +12,9 @@
 <link href="{{ asset('web/img/favicon.ico') }}" rel="icon">
 <!-- Google Web Fonts -->
 <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+{{-- <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet"> --}}
 <!-- Font Awesome -->
-<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" rel="stylesheet">
+<link href="{{ asset('web/css/all.min.css') }}" rel="stylesheet">
 <!-- Libraries Stylesheet -->
 <link href="{{ asset('web/lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
 <!-- Customized Bootstrap Stylesheet -->
